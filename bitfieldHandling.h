39ac5cd057539d862e38abf67955d4de4444e90b
Djee <ongola@free.fr>
2020-02-12 15:00:31 +0100

    Correction of Wrong Parameter Name

--- bitfieldHandling.h
+++ bitfieldHandling.h
@@ -11,9 +11,9 @@
 // returns value v with #n bits set to f, from bit #from (0 based)
 // example: v = 0xaa (10101010), f = 0x0a (1010)
 //          v = SET_BITS(v, f, 4, 3) => V = 0xd2 (11010010)
-#define SET_BITS(v, f, n, from) ((v) & ~(((1<<(n))-1)<<(from)) | (((b)&((1<<(n))-1))<<(from))) 
+#define SET_BITS(v, f, n, from) ((v) & ~(((1<<(n))-1)<<(from)) | (((f)&((1<<(n))-1))<<(from))) 
 
 // same as SET_BITS, and in addition assign result to v, assuming v is an LVALUE
-#define ASSIGN_BITS(v, b, n, from) v = SET_BITS(v, b, n, from)
+#define ASSIGN_BITS(v, f, n, from) v = SET_BITS(v, f, n, from)
 
 #endif
