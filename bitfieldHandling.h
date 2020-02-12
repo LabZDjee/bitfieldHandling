b3e2ac45650073e964082f921f931ff7bd56ba27
Djee <ongola@free.fr>
2020-02-12 14:41:07 +0100

    Clarification on SET_BITS Macro

--- bitfieldHandling.h
+++ bitfieldHandling.h
@@ -8,7 +8,7 @@
 #define TAKE_BITS(v, n, from) (((v) >> (from)) & ((1<<n) - 1)) 
 
 // returns value f to set to v, n bits wide set from bit #from (0 based)
-// example: v = 0xaa (10101010), b = 0x0a (1010)
+// example: v = 0xaa (10101010), f = 0x0a (1010)
 //          v = SET_BITS(v, b, 4, 3) => V = 0xd2 (11010010)
 #define SET_BITS(v, f, n, from) ((v) & ~(((1<<(n))-1)<<(from)) | (((b)&((1<<(n))-1))<<(from))) 
 
