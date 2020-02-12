08f324db821ee18065d3ec93843d8d06d7f2e7ec
Djee <ongola@free.fr>
2020-02-12 14:44:03 +0100

    Addition of Multiple Include Prevention in Header

--- bitfieldHandling.h
+++ bitfieldHandling.h
@@ -1,4 +1,5 @@
-
+#ifndef _BITFIELD_HANDLING_INCLUDED
+#define _BITFIELD_HANDLING_INCLUDED
 
 // bitwise macros on **unsigned** integer types 
 
@@ -9,8 +10,10 @@
 
 // returns value f to set to v, n bits wide set from bit #from (0 based)
 // example: v = 0xaa (10101010), f = 0x0a (1010)
-//          v = SET_BITS(v, b, 4, 3) => V = 0xd2 (11010010)
+//          v = SET_BITS(v, f, 4, 3) => V = 0xd2 (11010010)
 #define SET_BITS(v, f, n, from) ((v) & ~(((1<<(n))-1)<<(from)) | (((b)&((1<<(n))-1))<<(from))) 
 
 // same as SET_BITS, and in addition assign result to v, assuming v is an LVALUE
 #define ASSIGN_BITS(v, b, n, from) v = SET_BITS(v, b, n, from)
+
+#endif
